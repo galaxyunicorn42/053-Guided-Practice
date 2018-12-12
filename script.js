8967b9a89fc6d5b8f80a4b567f732f0bb7dc344b
galaxyunicorn42 <43255742+galaxyunicorn42@users.noreply.github.com>
2018-12-11 16:50:39 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,16 @@
+$("#replyYes").click(function() {
+    //Add a reply from Homer. Look for the div with #homers-reply.
+    $("#homers-reply").text("I love your donut, please, give me the donut,");
+    $("#homers-reply").css("color","green");
+});
+
+$("#replyNo").click(function() {
+    //Add a reply from Homer. Look for the div with #homers-reply.
+    $("#homers-reply").text("no, i dont want that,red");
+    $("#homers-reply").css("color","red"); 
+});
+  
+
+    
+    
+
